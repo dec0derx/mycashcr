8858ed792032297318f2c104ddf8f385e551226c
decøderx <decoderx@protonmail.com>
2025-03-15 17:49:09 -0600

    Security for key

--- economics/openai_requests.py
+++ economics/openai_requests.py
@@ -1,9 +1,17 @@
-#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
-
+import os
 import openai
 
 def check_exchange_rate(historical_exchange_rate_buy):
-    client = openai.OpenAI(api_key="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")  # Replace with your actual API key
+
+    OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+    print(OPENAI_API_KEY)
+    
+
+    if not OPENAI_API_KEY:
+        return "Insights are not available today."
+
+
+    client = openai.OpenAI(api_key=OPENAI_API_KEY) 
 
     response = client.chat.completions.create(
         model="gpt-4o",
@@ -11,5 +19,4 @@ def check_exchange_rate(historical_exchange_rate_buy):
     )
     
     return response.choices[0].message.content
-
 
